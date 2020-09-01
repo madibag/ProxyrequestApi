87c4b59fbbda49936231f080da09e6668091e686
madibag <62722024+madibag@users.noreply.github.com>
2020-09-01 18:53:55 +0300

    Update proxygrabber.py

--- proxygrabber.py
+++ proxygrabber.py
@@ -2,21 +2,12 @@
 
 from requests_html import HTMLSession
 #import script
-import random
-import json
 import time
 
 
 
 def make_request(url):
-	re = open('proxy.json', 'r').read()
-	pr_url = json.loads(re)
-	#
-	
 	session = HTMLSession()
-	leng = len(pr_url['data'])
-	rand = random.randint(1, leng)
-	proxy = pr_url['data'][rand]
 	r = session.get(url)
 	r.html.render()
 	co = (r.html)
